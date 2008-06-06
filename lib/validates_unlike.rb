f4a553da8163dc000ddaf7426565a03e633f0534
Sam Schroeder <samuelschroeder@gmail.com>
2008-06-06 08:05:40 -0500

    first commit

---
+++ lib/validates_unlike.rb
@@ -0,0 +1,16 @@
+module ActiveRecord
+  module Validations
+    module ClassMethods
+      def validates_unlike(*attr_names)
+        configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid], :on => :save, :with => nil }
+        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+
+        raise(ArgumentError, "A regular expression must be supplied as the :with option of the configuration hash") unless configuration[:with].is_a?(Regexp)
+
+        validates_each(attr_names, configuration) do |record, attr_name, value|
+          record.errors.add(attr_name, configuration[:message]) if value.to_s =~ configuration[:with]
+        end
+      end
+    end    
+  end
+end
